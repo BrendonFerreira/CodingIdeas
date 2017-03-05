5325f1381df8e9cc59f419d44185aca51147dd66
Brendon Ferreira <brendonferreiradm@gmail.com>
2017-03-05 02:19:43 -0300

    - Code refactoring

--- ServerFramework/usage.js
+++ ServerFramework/usage.js
@@ -1,67 +1,10 @@
 const futurename = require('futurename');
 const app = new futurename.App()
 
-// pipe usage 
-fn( request, response ){
-   return `The message is ${request.params.message}`;
-}  
-
-// example 2
-const mainRouter = (request, response) => {
-  switch( request.url ){
-    case "/home" : {
-      // The usage of function response may be optional
-      // but will not be able to apply pipe middlewheres
-      return response({ body : "<h1>Hello World</h1>" })
-    } 
-    case "/message" : {
-      return { body : "<h1>This is awesome</h1>" }
-    }
-    case "/help" : {
-      return response("I can not help you");
-    }
-    default : {
-      return apiRouter( request, response ); 
-    }
-  }
-};
-
-
-let Peoples = ['Brendon', 'Braian', 'Bruno'];
-const People = {
-   all : function(request, response){
-      return response( null , Peoples);
-   },
-   delete : function(request, response) {
-      Peoples.splice( request.params.id , -1);
-      return response( { ok : 1 } )
-   },
-   create : function(request, response){
-      Peoples.push( request.body );
-      return response( request.body );
-   }
-};
-
-const models = [
-   {
-      route : 'api/people',
-      method: 'get',
-      action : People.all
-   },{
-      route : 'api/people/:id',
-      method: 'delete',
-      action : People.delete
-   },{
-      route : 'api/people',
-      method: 'post',
-      action : People.create
-   } 
-];
-
-const modelsIndexer = (request, response)=>{
+const routesApiIndexer = (request, response)=>{
    // api/:model/action
    for( let index in models ) {
-      if( request.routeMatch( models[index].route ) ){
+      if( request.routeMatch( routes[index]. ) ){
          return models[index].action( request, response)     
       }
    }
@@ -71,7 +14,7 @@ const apiRouter = (request, response) => {
    return request
       .pipe( require('config/multerConfig') )
       .pipe( routeMatch() )
-      .pipe( modelsIndexModels() )
+      .pipe( routesApiIndexer() )
 }
 
 app.on('request', function(request){
